f357abe2ed2787e3c72781aa259f4f34c011d634
DNS <s.morgunova@mail.ru>
2017-01-23 22:44:08 +0700

    Первый коммит

---
+++ Shapes/src/ru/academits/morgunova/Main.java
@@ -0,0 +1,10 @@
+package ru.academits.morgunova;
+
+/**
+ * Created by DNS on 23.01.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
