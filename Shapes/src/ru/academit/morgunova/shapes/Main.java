c5ff62c5f84a91e84dc16e8e3e7079fbf2a6c5ce
DNS <s.morgunova@mail.ru>
2017-01-23 23:49:51 +0700

    Первый коммит

--- Shapes/src/ru/academit/morgunova/shapes/Main.java
+++ Shapes/src/ru/academit/morgunova/shapes/Main.java
@@ -1,10 +1,10 @@
-package ru.academits.morgunova;
+package ru.academit.morgunova.shapes;
 
 /**
  * Created by DNS on 23.01.2017.
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("hello world!");
     }
 }
